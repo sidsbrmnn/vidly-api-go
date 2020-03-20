c4cd1f078ce9c500758ab8865349aa3f2eef3e7f
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-03-20 23:24:22 +0530

    Created response writer methods

--- util.go
+++ util.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 	"time"
 )
 
 func dbContext(i time.Duration) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithTimeout(context.Background(), i*time.Second)
 	return ctx, cancel
+}
+
+func writeError(w http.ResponseWriter, statusCode int, message string) {
+	writeResponse(w, statusCode, map[string]string{"message": message})
+}
+
+func writeResponse(w http.ResponseWriter, statusCode int, v interface{}) {
+	response, _ := json.Marshal(v)
+
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(response)
 }
